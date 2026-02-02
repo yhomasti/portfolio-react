ff30bf8622a70dcfb8e91c0aa61b143e9430284f
yhomasti <yi.th@northeastern.edu>
2026-02-01 23:06:07 -0500

    moving down profile info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,29 +3,25 @@ function About() {
     <section id="about">
       <p className="section__text__p1">Learn more...</p>
       <h1 className="title">About Me!</h1>
-      <div className="section-container">
-        <div className="section__pic-container">
-          <img src="/assets/thomas-profile1-circle.png" alt="Profile picture" className="about-pic" />
+      
+      {/* Intro Section with WB Image + Bio */}
+      <div className="about-intro-section">
+        <div className="about-wb-container">
+          <img 
+            src="/assets/Thats all folks-modified.png" 
+            alt="Thomas Yi at Warner Bros" 
+            className="about-wb-image"
+          />
+          <p className="about-current-position">Currently: Software Engineer Intern at Warner Bros. Discovery</p>
         </div>
-        <div className="about-details-container">
-          <div className="about-containers">
-            <div className="details-container">
-              {/* Small centered icon */}
-              <img 
-                src="/assets/sock-profile.png" 
-                alt="Experience icon" 
-                style={{ 
-                  width: '50px', 
-                  height: '50px', 
-                  objectFit: 'contain',
-                  margin: '0 auto rem auto',
-                  display: 'block'
-                }}
-              />
-              <h3>About Thomas:</h3>
-              <p>I'm Thomas Yi, and I'm a student at Northeastern University pursuing a combined degree in Computer Science and Media Arts. I have a passion for telling stories and finding ways to express them, and I believe everyone should express their childish side every so often in their work. I am always looking for a chance to grow and learn more while pushing others and myself to be the best version of themselves that they can be.</p>
-            </div>
-          </div>
+
+        <div className="about-bio">
+          <p className="about-description">
+            I'm Thomas Yi, a student at Northeastern University pursuing a combined degree in Computer Science and Media Arts. 
+            I have a passion for telling stories and finding ways to express them, and I believe everyone should express 
+            their childish side every so often in their work. I am always looking for a chance to grow and learn more 
+            while pushing others and myself to be the best version of themselves that they can be.
+          </p>
         </div>
       </div>
     </section>
