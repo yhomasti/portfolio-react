2b64fc1ab26431c47f4511d89181de5b7e363b05
yhomasti <yi.th@northeastern.edu>
2026-04-09 09:12:09 -0400

    updating about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,9 +19,20 @@ function About() {
 
         <div className="about-bio">
           <p className="about-description">
-            Hello! I'm Thomas! I'm a 3D animator and technical artist with a passion for telling stories and bringing characters to life. I love finding creative ways to express ideas through animation, and I believe everyone should express
-            their childish side every so often in their work. I've gained industry experience in entertainment and developed a background in programming and pipeline development, and I wish to bridge the gap between art and technology in animation and games. I am always looking for a chance to grow and learn more
-            while pushing others and myself to be the best version of themselves that they can be.
+            Hello! I'm Thomas! I'm a 3D animator and technical artist with a passion for 
+            telling stories and bringing characters to life. I love finding creative ways 
+            to express ideas through animation, and I believe everyone should express 
+            their childish side every so often in their work.
+          </p>
+          <p className="about-description">
+            I've gained industry experience in entertainment and developed a background in 
+            programming and pipeline development, but I've always been fascinated in the 
+            world of animation and the creative side of production. Currently, I've been 
+            a Software Engineering intern with Warner Bros. Games, and having 
+            the chance to learn about the entertainment industry while picking up character animation 
+            mentorship from industry professionals has been invaluable for my growth, both as a programmer and an artist. 
+            I'm always looking for a chance to learn, and hope to see myself continue to 
+            grow with opportunities in the world of entertainment and animation, whether it be for a technical or creative role.
           </p>
         </div>
       </div>
