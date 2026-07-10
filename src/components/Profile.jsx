4ece94415b571a59f55c5a56b52f6342c10731d0
yhomasti <yi.th@northeastern.edu>
2026-07-10 16:58:13 -0700

    updating about

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -49,7 +49,7 @@ function Profile() {
               alt="Thomas Yi at Warner Bros" 
               className="about-wb-image"
             />
-            <p className="about-current-position">Currently: Tools Programming Intern at Warner Bros. Games</p>
+            <p className="about-current-position">Previously: Tools Programming Intern at Warner Bros. Games</p>
         </div>
       </div>
 
